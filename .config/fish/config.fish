48e774dc309316034c601c098c025153b09e0c47
Nate Gadzhi <nate@respawn.io>
2022-11-13 14:40:38 -0800

    Updated fish config for gpg agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,6 @@ set EDITOR nvim
 set VISUAL nvim
 set GIT_EDITOR nvim
 
-# When GPG wants the key passphrase, but can't figure out which TTY to use to get it.
-set -xg GPG_TTY (tty)
-set -e SSH_AGENT_PID
-set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
 
 # LC_CTYPE bug
 set LC_CTYPE "en_US.UTF-8"
@@ -20,6 +15,12 @@ if [ -d "/opt/homebrew/sbin" ]; set -g fish_user_paths "/opt/homebrew/sbin" $fis
 
 if [ -d "/usr/local/go/bin" ]; set -g fish_user_paths "/usr/local/go/bin" $fish_user_paths; end
 if [ -d (echo ~)"/go/bin" ]; set -g fish_user_paths (echo ~)"/go/bin" $fish_user_paths; end
+
+# When GPG wants the key passphrase, but can't figure out which TTY to use to get it.
+set -xg GPG_TTY (tty)
+set -e SSH_AGENT_PID
+set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
 
 # rbenv
 if test -d ~/.rbenv
