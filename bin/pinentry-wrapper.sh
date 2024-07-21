bf1f2f31602cae7f675095b4ced750c44deb503d
Natik Gadzhi <natik@respawn.io>
2024-07-20 20:28:37 -0700

    ZED: Ignore prompts, update config

--- bin/pinentry-wrapper.sh
+++ bin/pinentry-wrapper.sh
@@ -3,13 +3,13 @@
 # Determine if we're in a terminal or a GUI application
 if [[ -n "$SSH_TTY" ]]; then
     # Running in a remote terminal
-    /opt/homebrew/bin/pinentry "$@"
+    /opt/homebrew/bin/pinentry-mac "$@"
 elif [[ -n "$TMUX" || -n "$STY" ]]; then
     # Running in a terminal multiplexer
-    /opt/homebrew/bin/pinentry "$@"
+    /opt/homebrew/bin/pinentry-mac "$@"
 elif [[ -n "$GPG_TTY" ]]; then
     # GPG_TTY is set, indicating terminal use
-    /opt/homebrew/bin/pinentry "$@"
+    /opt/homebrew/bin/pinentry-mac "$@"
 else
     # Assume GUI environment otherwise
     /opt/homebrew/bin/pinentry-mac "$@"
