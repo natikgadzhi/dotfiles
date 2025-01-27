c92c6ffc62680a29c821f4195e1c4c5a91930d67
Natik Gadzhi <natik@respawn.io>
2025-01-26 19:28:58 -0800

    Fish: yep, jenv support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -94,11 +94,11 @@ set -gx VOLTA_HOME "$HOME/.volta"
 # TODO: Spicy, Volta might not be there.
 set -gx PATH "$VOLTA_HOME/bin" $PATH
 
-# TODO: Should be removed in favor of jenv?
-if [ -d /opt/homebrew/opt/openjdk/ ];
-  set -gx JAVA_HOME /opt/homebrew/opt/openjdk
-  set -gx PATH $JAVA_HOME/bin $PATH
-end
+# # TODO: Should be removed in favor of jenv?
+# if [ -d /opt/homebrew/opt/openjdk/ ];
+#   set -gx JAVA_HOME /opt/homebrew/opt/openjdk
+#   set -gx PATH $JAVA_HOME/bin $PATH
+# end
 
 # Wasmer
 export WASMER_DIR="/Users/natikgadzhi/.wasmer"
